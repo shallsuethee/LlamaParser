10b5f873d1f454f8d4aa3cf223418dc6da82a427
Milan <mitka.milan@gmail.com>
2011-12-27 16:54:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,31 +4,5 @@
 <title>Parser</title>
 </head>
 <body>
-
-<form action="index.php" method="post">
-<textarea autofocus name="vstup" rows="20" cols="80"></textarea><br />
-<input type="submit" value="Potvrdiť" />
-</form>
-<br />
-
-<?php
-	if(isset($_POST['vstup']))
-	{
-		$text = $_POST['vstup'];
-		$text = stripslashes($text);
-
-		$order = array("\r\n", "\n", "\r");
-		$replace = '';
-		$text = str_replace($order, $replace, $text);
-		$text = preg_replace("#\<en id=\"(.+)\"\>\<p class=\"main\"\>\<b0\>(.+)\</b0\>#", '<d:entry id="$1" d:title="$2"><d:index d:value="$2" /><p class="main"><h1>$2</h1>', $text);
-		$text = preg_replace("#\</en#", '</d:entry', $text);
-
-		echo $text;
-	}
-	else
-	{
-			
-	}
-?>
 </body>
 </html>
